f59318e385730e0432d332bc46889d242593440c
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-02-12 13:04:28 +0100

    null test

--- src/creational/factorymethod/Main.java
+++ src/creational/factorymethod/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		// instantiation by Factory method without specifying the exact class to create
+		// the factory does not know which subclass to create, it is told by parameter
 		Car car1 = CarFactory.getCar(CarType.SUV);
 		Car car2 = CarFactory.getCar(CarType.TOURISM);
 
@@ -21,6 +22,10 @@ public class Main {
 		// different implementation of the same method in each subclass
 		System.out.println(car1.getFinalPrice());
 		System.out.println(car2.getFinalPrice());
+
+		// null test
+		Car car3 = CarFactory.getCar(null);
+		System.out.println(car3);
 
 	}
 
