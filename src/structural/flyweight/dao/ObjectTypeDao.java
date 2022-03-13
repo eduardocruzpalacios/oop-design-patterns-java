21dc4789238891a8a42e0fcc230731b8c30eb012
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-03-13 10:00:10 +0100

    Flyweight DAO factory of the Flyweight design pattern

---
+++ src/structural/flyweight/dao/ObjectTypeDao.java
@@ -0,0 +1,26 @@
+package structural.flyweight.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import structural.flyweight.flyweight.ObjectType;
+
+public class ObjectTypeDao {
+
+	private static Map<String, ObjectType> objects;
+
+	private static void loadObject() {
+		// let's suppose we are getting our flyweights from a ddbb
+		objects = new HashMap<String, ObjectType>();
+		ObjectType objectType = new ObjectType("Cheap chair", "cheap_chair.gif", 45);
+		objects.put(objectType.getName(), objectType);
+		objectType = new ObjectType("Regular shower", "regular_shower.gif", 100);
+		objects.put(objectType.getName(), objectType);
+	}
+
+	public static Map<String, ObjectType> getObjects() {
+		loadObject();
+		return objects;
+	}
+
+}
